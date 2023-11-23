7ff99757833f5f44fc63d86c62ecbb0f4e49337c
unknown <anton.haglund@iths.se>
2023-11-23 20:53:04 +0100

    Added Navigation

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -15,13 +15,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
@@ -44,91 +54,93 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.app.ui.theme.AppTheme
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "SuspiciousIndentation")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val painter =  painterResource(id = R.drawable.black_rashguard)
-            val description =  "Rashguard"
-            val title =  "Black Rashguard"
-
-            val snackbarHostState = remember { SnackbarHostState() }
+            val navController = rememberNavController()
             val scope = rememberCoroutineScope()
+            val snackbarHostState = remember { SnackbarHostState() }
+
+            Scaffold(
 
 
-                Scaffold(
-                    snackbarHost = { SnackbarHost(snackbarHostState) },
-                    content = {
-
-            CartSection()
-
-            Column(modifier = Modifier
+                bottomBar = {
 
 
-                    .fillMaxWidth()
+                    BottomNavigationBar(
+                        items = listOf(
+                            BottomNavItem(
+                                name = "Home", route = "home", icon = Icons.Default.Home
+
+                            ),
+                            BottomNavItem(
+                                name = "Chat", route = "chat", icon = Icons.Default.Notifications
+
+                            ),
+                            BottomNavItem(
+                                name = "Settings", route = "settings", icon = Icons.Default.Settings
+
+                            ),
+
+                            ), navController = navController, onItemClick = {
 
 
-                    .padding(16.dp)) {
-
-
-                ImageCard(
-                        painter = painter,
-                        contentDescription = description ,
-                        title = title,
-                        modifier = Modifier.padding(top = 70.dp),
-                        onAddToCartClicked = {
-                                scope.launch {
-
-                                    snackbarHostState.showSnackbar(
-
-                                        message = "The rashguard have been added to the cart",
-                                        actionLabel = "Undo"
-
-                                    )
-
-                                }
-
-
+                            navController.navigate(it.route)
                         }
 
-
-                )
-                
-                Spacer(modifier = Modifier.height(50.dp))
-                SubscribeBox(
-                    onSubscribeclicked = {
-                        scope.launch { snackbarHostState.showSnackbar(
-
-                            message = "Success!"
-                        ) }
+                    )
+                },
 
 
+                content = {
+
+                    NavHost(navController = navController, startDestination = "home" ) {
+
+
+                        composable("home")  {
+
+                            HomeScreen(
+
+                                description = "Rashguard",
+                                title = "Black Rashguard",
+                                snackbarHostState = snackbarHostState,
+                                )
+                        }
+                        composable("chat")  {
+
+                            ChatScreen()
+
+                        }
+                        composable("settings")  {
+
+                            SettingsScreen()
+                        }
 
                     }
 
 
+                }
+
+            )
 
 
-                )
-
-            }
-
-
-
-
-        }
-
-                )
-
-
-
+            //Navigation(navController = navController)
         }
     }
 }
@@ -136,40 +148,183 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CartSection() {
 
-val cartPainter = painterResource(id = R.drawable.cart)
-val cartDescription = "Shopping cart"
-val cartTitle = "Your Cart"
+    val cartPainter = painterResource(id = R.drawable.cart)
+    val cartDescription = "Shopping cart"
+    val cartTitle = "Your Cart"
 
-        Box(modifier = Modifier
+    Box(modifier = Modifier
 
-            .fillMaxSize()
-            .padding(16.dp),
-            contentAlignment = Alignment.TopEnd
-
-
-
-        ) {
-
-            Image(painter = cartPainter,
-                contentDescription =  cartDescription,
-                modifier = Modifier
-                   // .fillMaxWidth()
-                    .size(45.dp)
+        .fillMaxSize()
+        .padding(16.dp),
+        contentAlignment = Alignment.TopEnd
 
 
 
+    ) {
+
+        Image(painter = cartPainter,
+            contentDescription =  cartDescription,
+            modifier = Modifier
+                // .fillMaxWidth()
+                .size(45.dp)
+
+
+
+        )
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    NavigationBar(
+
+        modifier = Modifier,
+        containerColor = Color.DarkGray,
+
+        tonalElevation = 5.dp
+
+    ) {
+        items.forEach { item ->
+
+            val selected = item.route == backStackEntry.value?.destination?.route
+            NavigationBarItem(
+
+                selected = selected,
+                onClick = { onItemClick(item) },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Black,
+                    unselectedIconColor = Color.Gray
+                ),
+                icon = {
+
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+
+                        if (item.badgeCount > 0) {
+
+                            BadgedBox(badge = {
+                                Text(text = item.badgeCount.toString())
+                            }
+                            ) {
+
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+                            }
+                        } else {
+
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.name
+                            )
+                        }
+                        if(selected) {
+
+                            Text(text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp
+
+                            )
+
+                        }
+                    }
+
+                }
             )
 
         }
 
+    }
+
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+
+    description: String,
+    title: String,
+    snackbarHostState: SnackbarHostState,
+
+) {
+    val scope = rememberCoroutineScope()
+    val painter = painterResource(id = R.drawable.black_rashguard)
+    CartSection()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+
+
+        ImageCard(
+            painter = painter,
+            contentDescription = description,
+            title = title,
+            modifier = Modifier.padding(top = 70.dp),
+            onAddToCartClicked = {
+                scope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = "The rashguard have been added to the cart",
+                        actionLabel = "Undo"
+                    )
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+        SubscribeBox(
+            onSubscribeclicked = {
+                scope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = "Success!"
+                    )
+                }
+            }
+        )
+    }
+    SnackbarHost(
+        hostState = snackbarHostState,
+
+        ) { data ->
+          Snackbar(
+              snackbarData = data,
+              )}
+}
+
+
+@Composable
+fun ChatScreen () {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+
+    ) {
+
+    }
+}@Composable
+fun SettingsScreen () {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+
+    ) {
 
 
     }
-
-
-
-
-
+}
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -237,17 +392,13 @@ fun ImageCard(
                 )
             }
 
-
         }
-
-
-
 
 
         Button(
             onClick = {
 
-                      onAddToCartClicked()
+                onAddToCartClicked()
 
             },
             colors = ButtonDefaults.buttonColors(Color.Black),
@@ -263,16 +414,10 @@ fun ImageCard(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-
-
-
-
-
-        }
-
-
-
     }
+
+
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SubscribeBox(onSubscribeclicked: () -> Unit) {
@@ -326,7 +471,7 @@ fun SubscribeBox(onSubscribeclicked: () -> Unit) {
             Button(
                 onClick = {
 
-                        onSubscribeclicked()
+                    onSubscribeclicked()
                 },
                 colors = ButtonDefaults.buttonColors(Color.Black),
                 modifier = Modifier
@@ -337,23 +482,6 @@ fun SubscribeBox(onSubscribeclicked: () -> Unit) {
             ) {
                 Text(text = "Submit")
             }
-
-
-
-
         }
-
-
     }
-
 }
-//        Snackbar {
-//
-//            Text(text = "Subscribe to get updates")
-//        }
-
-
-
-
-
-
